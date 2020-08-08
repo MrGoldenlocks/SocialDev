3dadb0dc6e6eb1cbec88b607accc6a9f537e5754
Daniel Murphree <dm5w@mtmail.mtsu.edu>
2020-08-08 15:33:18 -0500

    user can now register and log in

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -30,10 +32,10 @@ router.post(
     try {
       let user = await User.findOne({ email });
 
-      if (user) {
-        res.status(400).json({ errors: [{ msg: 'User name taken' }] });
-      }
       // see if user exists
+      if (user) {
+        return res.status(400).json({ errors: [{ msg: 'User name taken' }] });
+      }
 
       // get user gravatar
       const avatar = gravatar.url(email, {
@@ -42,6 +44,7 @@ router.post(
         d: 'mm',
       });
 
+      // create user
       user = new User({
         name,
         email,
@@ -50,7 +53,6 @@ router.post(
       });
 
       // encrypt password
-
       const salt = await bcrypt.genSalt(10);
 
       user.password = await bcrypt.hash(password, salt);
@@ -58,8 +60,21 @@ router.post(
       await user.save();
 
       // return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
 
-      res.send('user registered');
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 350000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server Error');
