1152b5a4a6484e5e00cd677d60876f305fc07499
Daniel Murphree <murphree.daniel@gmail.com>
2020-08-21 00:51:17 -0500

    profile page completed

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -8,7 +8,7 @@ import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
 import ProfileExperience from './ProfileExperience';
 import ProfileEducation from './ProfileEducation';
-// import ProfileGithub from './ProfileGithub';
+import ProfileGithub from './ProfileGithub';
 
 const Profile = ({ getProfileById, profile: { profile }, auth, match }) => {
   useEffect(() => {
@@ -65,6 +65,9 @@ const Profile = ({ getProfileById, profile: { profile }, auth, match }) => {
                 <h4>No education credentials</h4>
               )}
             </div>
+            {profile.githubusername && (
+              <ProfileGithub username={profile.githubusername} />
+            )}
           </div>
         </Fragment>
       )}
